52b76869d16c72ec12ae14ec191596771677c252
AlistairCoates <alistairescoates@gmail.com>
2019-11-05 09:02:16 +0000

    Created new functions.

--- lyric_counter/__main__.py
+++ lyric_counter/__main__.py
@@ -5,35 +5,34 @@ from requests.exceptions import HTTPError
 from json.decoder import JSONDecodeError
 import numpy as np
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('artist', type=str, nargs='+', help='The artist name')
-    args = parser.parse_args()
-    musicbrainzngs.set_useragent('lyric-counter', '0.1.0')
 
-    # Get artist id
-    print(musicbrainzngs.search_artists(query=args.artist, limit=1))
-    result = musicbrainzngs.search_artists(sortname=args.artist, limit=1)
+def get_artist_details(artist):
 
+    result = musicbrainzngs.search_artists(sortname=artist, limit=1)
     artistid = result['artist-list'][0].get('id')
-    print(artistid)
     artistname = result['artist-list'][0].get('name')
 
+    return artistid, artistname
+
+
+def get_song_list(id):
+
     # Get list of song titles
-    titles = []
-    works = musicbrainzngs.search_works(arid=artistid, limit=100)
-    songs = works.get('work-list')
-    if not songs:
-        return
-    for song in songs:
-        titles.append(song.get('title'))
+    works = musicbrainzngs.search_works(arid=id, limit=100)
     work_count = works.get('work-count')
-    pages = work_count//100
-    for x in range(pages):
-        offset = (x+1)*100
-        works = musicbrainzngs.search_works(arid=artistid, limit=100, offset=offset)
+    pages = int(np.ceil(work_count / 100.0))
+    songs = works.get('work-list')
+    titles = [song.get('title') for song in songs]
+
+    for page in range(1, pages):
+        offset = page * 100
+        works = musicbrainzngs.search_works(arid=id, limit=100, offset=offset)
         for song in works.get('work-list'):
             titles.append(song.get('title'))
+    return titles
+
+
+def count_lyrics(titles, artistname):
 
     # Count lyrics of each song
     lyric_count = []
@@ -45,6 +44,16 @@ def main():
                 lyric_count.append(lyrics)
         except (HTTPError, JSONDecodeError):
             pass
+    return lyric_count
+
+
+def calculate_statistics(artist):
+
+    artistid, artistname = get_artist_details(artist)
+
+    titles = get_song_list(artistid)
+
+    lyric_count = count_lyrics(titles, artistname)
 
     # Caluculate statistics
     if lyric_count:
@@ -59,6 +68,15 @@ def main():
         print('min: {}'.format(min))
         print('max: {}'.format(max))
 
-    #print(works)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('artists', type=str, nargs='+', help='The artist name')
+    args = parser.parse_args()
+    artists = " ".join(args.artists).split(',')
+    musicbrainzngs.set_useragent('lyric-counter', '0.1.0')
+    for artist in artists:
+        calculate_statistics(artist)
+
 if __name__ == '__main__':
    main()
